0851e45921acd2cbab39a5420d79fbc8b978ae77
jonBarcus <barcus.j@gmail.com>
2017-07-03 18:51:50 -0400

    made additions to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authenticate_user!
+    current_user.present?
+  end
+
+  def current_user
+    super || User.where(soft_token: soft_token).first_or_initialize
+  end
+
+  private
+
+  def soft_token
+    session[:user_token] ||= SecureRandom.hex(8)
+  end
 end
